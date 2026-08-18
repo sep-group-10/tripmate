2d03352cfe403afaf25f753c570c950025494836
kajatheepan <kajatheepan0311@gmail.com>
2026-08-18 17:59:55 +0530

    fix: fixed configuring backend baseUrl

--- mobile/lib/utils/constants.dart
+++ mobile/lib/utils/constants.dart
@@ -1,2 +1,6 @@
-const String baseUrl = 'http://10.0.2.2:8000';
+const String baseUrl = String.fromEnvironment(
+  'API_BASE_URL',
+  defaultValue: 'http://localhost:8000',
+);
+
 const int requestTimeoutSeconds = 30;
