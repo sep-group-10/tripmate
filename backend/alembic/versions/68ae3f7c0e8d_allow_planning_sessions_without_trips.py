4052d45ee5f1c548747456fda985173e6444a4e3
babijana <babijanaj.23@cse.mrt.ac.lk>
2026-09-22 09:15:17 +0530

    fix(lint): use collections abc sequence

--- backend/alembic/versions/68ae3f7c0e8d_allow_planning_sessions_without_trips.py
+++ backend/alembic/versions/68ae3f7c0e8d_allow_planning_sessions_without_trips.py
@@ -6,11 +6,11 @@ Create Date: 2026-09-19 03:39:15.705107
 
 """
 
-from typing import Sequence
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "68ae3f7c0e8d"
